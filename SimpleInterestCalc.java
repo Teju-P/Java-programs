3de72f8b8d9e7027a385035f266e07207f147e61
TEJU P <115681017+Teju-P@users.noreply.github.com>
2024-11-21 11:04:25 +0530

    Updated simpleInterestCalculator 211120241104AM

--- SimpleInterestCalc.java
+++ SimpleInterestCalc.java
@@ -4,11 +4,13 @@ public class SimpleInterestCalc {
     BigDecimal principal, interest;
     SimpleInterestCalc(String principal, String interest){
         this.principal = new BigDecimal(principal);
-        this.interest = new BigDecimal(interest);
+        this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
     }
 
     BigDecimal calcTotalValue(int noOfYears){
         BigDecimal numberOfYears = new BigDecimal(noOfYears);
-        
+        // totalValue = principal + principal * interest * noOfYears;
+        BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(numberOfYears));
+        return totalValue;
     }
 }
--- SimpleInterestCalcRunner.java
+++ SimpleInterestCalcRunner.java
@@ -1,5 +1,9 @@
+import java.math.BigDecimal;
+
 class SimpleInterestCalcRunner{
     public static void main(String[] args) {
-        SimpleInterestCalc x = new SimpleInterestCalc();
+        SimpleInterestCalc x = new SimpleInterestCalc("4500.00","7.5");
+        BigDecimal total = x.calcTotalValue(5);
+        System.out.println(total);
     }
 }
